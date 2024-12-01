0c4f48abdc30b6c472f74a2bf57e6d759fcf9e5f
Peter De-Ath <peter@de-ath.co.uk>
2024-12-01 01:26:37 +0000

    feat: add document upload functionality and enhance file acceptance criteria

--- src/lib/components/chat/Chat.svelte
+++ src/lib/components/chat/Chat.svelte
@@ -12,7 +12,7 @@
 
 	import { get, type Unsubscriber, type Writable } from 'svelte/store';
 	import type { i18n as i18nType } from 'i18next';
-	import { WEBUI_BASE_URL } from '$lib/constants';
+	import { WEBUI_BASE_URL, WEBUI_API_BASE_URL } from '$lib/constants';
 
 	import {
 		chatId,
@@ -85,6 +85,7 @@
 	import Placeholder from './Placeholder.svelte';
 	import NotificationToast from '../NotificationToast.svelte';
 	import Spinner from '../common/Spinner.svelte';
+	import { uploadFile } from '$lib/apis/files';
 
 	import { openDB } from 'idb';
 
@@ -1904,6 +1905,53 @@
 					status: 'uploaded'
 				}))
 			];
+			return;
+		}
+
+		if(shareData.documents) {
+			const tempItemId = uuidv4();
+			const fullContext = true;
+
+			const fileItem = {
+				type: 'file',
+				file: '',
+				id: null,
+				url: '',
+				name: shareData.documents.name,
+				collection_name: '',
+				status: 'uploading',
+				size: shareData.documents.size,
+				error: '',
+				itemId: tempItemId,
+				...(fullContext ? { context: 'full' } : {})
+			};
+			files = [...files, fileItem];
+
+			// convert the base64 back to binary
+			const base64Data = shareData.documents.data.split(',')[1];
+			const byteArray = Uint8Array.from(atob(base64Data), char => char.charCodeAt(0));
+
+			const document = new File([byteArray], shareData.documents.name, {
+				type: shareData.documents.type
+			});
+
+			// Upload using the files api
+			const uploadedFile = await uploadFile(localStorage.token, document);
+			if (uploadedFile) {
+				if (uploadedFile.error) {
+					toast.warning(uploadedFile.error);
+				}
+
+				fileItem.status = 'uploaded';
+				fileItem.file = uploadedFile;
+				fileItem.id = uploadedFile.id;
+				fileItem.collection_name = uploadedFile?.meta?.collection_name;
+				fileItem.url = `${WEBUI_API_BASE_URL}/files/${uploadedFile.id}`;
+
+				files = files;
+			} else {
+				files = files.filter((item) => item?.itemId !== tempItemId);
+			}
 			return;
 		}
 
